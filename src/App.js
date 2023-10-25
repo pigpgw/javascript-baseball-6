4e1f656f6f18434761071bbab094cd5c5bf72b96
ceh20002@naver.com <ceh20002@naver.com>
2023-10-25 22:42:29 +0900

    message

--- src/App.js
+++ src/App.js
@@ -28,7 +28,6 @@ class App {
     }
   }
 
-  // 왜 while로 안하면 2번에 끊기는지 이해하기 
   async retry() {
       this.input = '';
       await this.getUserInput();
@@ -54,7 +53,6 @@ class App {
     MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
   }
   
-  //힌트 제공 기능 - 스트라이크 개수와 볼 개수 출력 {strike, ball} 가져와 표현
   printHintMessage(input) {
     if (input.strike === 0 && input.ball === 0){
       MissionUtils.Console.print("낫싱");
@@ -73,22 +71,20 @@ class App {
     }
   }
 
-  // 게임 시작 알리는 문구
   gameStartText() {
       MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
   }
 
-  // 사용자의 입력값이 유효한 값인지 체크 - 숫자인지, 3자리인지, 중복됬는지
   async getUserInput() {
     this.input = await MissionUtils.Console.readLineAsync('숫자를 입력해주세요 : ');
   }
 
   checkValidInput(input) {
     if (!this.isThreeNumber(input)) {
-      throw new Error("[ERROR]:Is not ThreeNumber Input");
+      throw new Error("[ERROR]: 1부터 9까지의 숫자 3자리를 입력해 주세요.");
     }
     if (!this.isSameNumber(input)) {
-      throw new Error("[ERROR]:Is Duplicate Input");
+      throw new Error("[ERROR]: 중복된 수 입니다.");
     }
     this.input = input.split('').map(Number);
     return this.input;
@@ -104,7 +100,6 @@ class App {
     return (checkSame.length === compareSame.size);
   }
 
-  // 스트라이크 볼 체크 기능 구현
   checkStrikeBall(userInput,computerInput){
     let strike = this.checkStrike(userInput,computerInput);
     let ball = this.checkBall(userInput,computerInput);
